12687e70820ee126ca41bc63a0988779fd9046c0
Razi D <racieldiz@gmail.com>
2020-06-24 23:42:56 -0700

    Updates

---
+++ precache-manifest.06f4664016209e42cc8338768416e62c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39da790e98b6f705047e91b9e86aa4ab",
+    "url": "/MadDogFlash/Budget-TypeScript/index.html"
+  },
+  {
+    "revision": "26e27652227cba4f2695",
+    "url": "/MadDogFlash/Budget-TypeScript/static/css/main.ae4e6770.chunk.css"
+  },
+  {
+    "revision": "8d1370e639a03df48e1b",
+    "url": "/MadDogFlash/Budget-TypeScript/static/js/2.4f09def5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MadDogFlash/Budget-TypeScript/static/js/2.4f09def5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26e27652227cba4f2695",
+    "url": "/MadDogFlash/Budget-TypeScript/static/js/main.c2f280f0.chunk.js"
+  },
+  {
+    "revision": "04e3a1e3d9b5b789b7e4",
+    "url": "/MadDogFlash/Budget-TypeScript/static/js/runtime-main.a1c2fb48.js"
+  }
+]);
